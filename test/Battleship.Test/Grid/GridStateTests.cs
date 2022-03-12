b2067300a4b73c3786e4a744cf907af24dcd012e
Richard Clacksman <rc@wordwall.co.uk>
2022-03-12 19:42:33 +0000

    Added test to check adding a second ship.

--- test/Battleship.Test/Grid/GridStateTests.cs
+++ test/Battleship.Test/Grid/GridStateTests.cs
@@ -19,7 +19,7 @@ namespace Battleship.Test.Grid
 
         [Theory]
         [MemberData(nameof(GetGridPositions))]
-        public void TryPlaceShip(bool expected, uint x, uint y, Direction direction, uint length)
+        public void TryPlaceShip_InEmptyGrid(bool expected, uint x, uint y, Direction direction, uint length)
         {
             var grid = new GridState(10, 10);
 
@@ -38,6 +38,54 @@ namespace Battleship.Test.Grid
             }
         }
 
+        [Theory]
+        // ships don't even touch
+        [InlineData(true, 5, 5, 4, Direction.Right, 5, 8, 4, Direction.Right)]
+        [InlineData(true, 0, 0, 4, Direction.Right, 6, 9, 4, Direction.Right)]
+
+        //Ships don't overlap
+        [InlineData(true, 5, 5, 4, Direction.Right, 5, 6, 4, Direction.Right)]
+        [InlineData(true, 0, 5, 4, Direction.Right, 4, 5, 4, Direction.Right)]
+        [InlineData(true, 0, 5, 4, Direction.Right, 4, 5, 4, Direction.Down)]
+
+        //ships in same direction
+        [InlineData(false, 5, 5, 4, Direction.Right, 5, 5, 4, Direction.Right)] //right on top
+        [InlineData(false, 5, 5, 4, Direction.Right, 7, 5, 3, Direction.Right)]
+        [InlineData(false, 7, 5, 3, Direction.Right, 5, 5, 4, Direction.Right)]
+
+        //crossed ships
+        [InlineData(false, 5, 5, 4, Direction.Right, 6, 4, 4, Direction.Down)]
+        public void TryPlaceShip_WithExistingShip(
+            bool canPlaceShip2,
+            uint x1, uint y1, uint length1, Direction direction1,
+            uint x2, uint y2, uint length2
+, Direction direction2)
+        {
+            var grid = new GridState(10, 10);
+
+            var ship1 = grid.TryPlaceShip(x1, y1, direction1, length1);
+            Assert.NotNull(ship1);
+            Assert.Contains(ship1, grid.Ships);
+
+            var ship2 = grid.TryPlaceShip(x2, y2, direction2, length2);
+
+            if (canPlaceShip2)
+            {
+                Assert.NotNull(ship2);
+                Assert.Contains(ship1, grid.Ships);
+                Assert.Contains(ship2, grid.Ships);
+            }
+            else
+            {
+                Assert.Null(ship2);
+                Assert.Single(grid.Ships, s => s == ship1);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Each call returns an object array meaning {shouldBeAbleToPlaceShip, y, y, direction, shipLength}</returns>
         public static IEnumerable<object[]> GetGridPositions()
         {
             //In grid, short enough
